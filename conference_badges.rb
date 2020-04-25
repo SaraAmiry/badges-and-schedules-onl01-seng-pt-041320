cf0d3f5efc13a9793c37f824475e2d8f57f6cbdc
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 05:44:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
-def batch_badge_creator(names_array)
-  
+def batch_badge_creator(speaker)
+  speaker = ["Edsgar", "Ada", "Charles", "Grace", "Linus", "Matz"]
+  speaker.each do {|name| puts "Hello, my name is #{name}"
   
 end
 

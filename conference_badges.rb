339c45144e013295afcd308ec882e2b2256142c6
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 05:44:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 def batch_badge_creator(speaker)
   speaker = ["Edsgar", "Ada", "Charles", "Grace", "Linus", "Matz"]
   speaker.each do {|name| puts "Hello, my name is #{name}"
-  
 end
 
 def assign_rooms
